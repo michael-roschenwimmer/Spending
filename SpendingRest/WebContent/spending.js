ed9a5faa91bf3fcebc6f8792235fdce4f45658c8
Michael Roschenwimmer <michael.roschenwimmer@gmail.com>
2017-06-25 14:01:30 -0600

    working

--- SpendingRest/WebContent/spending.js
+++ SpendingRest/WebContent/spending.js
@@ -1,22 +1,182 @@
 $(document).ready(function() {
   console.log("loaded");
-  loadSpendingTrackers();
+  startUp();
 
 });
 
-
-
-
-var loadSpendingTrackers = function() {
+var startUp = function() {
 	$.ajax({
 		type : "GET",
 		url : "rest/trackers",
 		dataType : "json",
 	}).done(function(data, status) {
 		console.log(data);
-		displayTable(data);
+		buildList(data);
 	}).fail(function(xhr, status, error) {
 		console.log('It blew up');
 		console.log(error);
 	});
 };
+
+var buildList = function(data) {
+    var table = $('<table>');
+    var thead = $('<thead>');
+    var tr = $('<tr>');
+    var th = $('<th>').text("Restaurant Bills");
+    tr.append(th);
+    thead.append(tr);
+    table.append(thead);
+    data.forEach(function(tracker, idx, array) {
+        if (idx % 2) {
+            var tr = $('<tr>').css("background-color", "#ff9999");
+        } else {
+            var tr = $('<tr>')
+        }
+        var tbody = $('<tbody>');
+        var td = $('<td>');
+        var input = $('<button>');
+        input.addClass('button');
+        input.attr('id', tracker.id);
+        input.text('Description');
+        input.on('click', function() {
+            var myReq = $.ajax({
+                type: "GET",
+                url: "rest/trackers/" + $(this).attr('id'),
+                dataType: "json"
+            });
+            myReq.done(function(data, status) {
+                console.log(data);
+                console.log(status);
+                $("#content").empty();
+                buildDesc(data);
+            });
+            myReq.fail(function(xhr, status, error) {
+                console.log('It blew up again');
+                console.log(error);
+            });
+        })
+        var deleteItem = $('<button>');
+        deleteItem.addClass('button');
+        deleteItem.attr('id', tracker.id);  // Remind
+        deleteItem.text('Delete Restaurant Bill');
+        deleteItem.on('click', function() {
+            var myReq = $.ajax({
+                type: "DELETE",
+                url: "rest/trackers/" + $(this).attr('id'),
+            });
+            myReq.done(function(data, status) {
+                console.log(data);
+                console.log(status);
+                $("#content").empty();
+
+                var tablediv = $("<div>");
+                tablediv.attr('id', 'table');
+                $("#content").append(tablediv);
+                startUp();
+            });
+            myReq.fail(function(xhr, status, error) {
+                console.log('It blew up again');
+                console.log(error);
+            });
+        })
+        td.text(tracker.id + ". " + tracker.restaurantName);
+        tr.append(td);
+        tr.append(input);
+        tr.append(deleteItem);
+        tbody.append(tr);
+        table.append(tbody);
+    })
+    $('#table').append(table);
+
+    var createRestaurantBill = $("<h3>");
+    createRestaurantBill.text("Create a new Bill");
+    $("#table").append(createRestaurantBill);
+
+    var restaurantName = $("<input/>").attr({
+        type: "text",
+        id: "restaurantName",
+        placeholder: "Restaurant Name"
+    }).appendTo("#table");
+    var billAmount = $("<input/>").attr({
+        type: "text",
+        id: "bill",
+        placeholder: "Bill Amount in Dollars"
+    }).appendTo("#table");
+
+    var createRestaurantBillButton = $('<button>');
+    createRestaurantBillButton.addClass('button').text("Create New Bill").appendTo("#table");
+
+
+
+    createRestaurantBillButton.on('click', function() {
+        var restaurantBillObj = {
+            restaurantName: $("#restaurantName").val(),
+            bill: $("#bill").val(),
+
+        };
+        console.log(restaurantBillObj);
+
+        var myPost = $.ajax({
+            type: "POST",
+            url: "rest/trackers",
+            dataType: "json",
+            contentType: 'application/json',
+            data: JSON.stringify(restaurantBillObj)
+        });
+
+
+        myPost.done(function(data, status) {
+            console.log(data);
+            console.log(status);
+            // start(buildList);  //check and fix?
+
+            // $("#content").empty();
+
+            $("body").empty();
+            buildDesc(data);
+        });
+        myPost.fail(function(xhr, status, error) {
+            console.log('It blew up again');
+            console.log(error);
+        });
+        $("#table").append(createRestaurantBillButton);
+    })
+
+
+}
+
+var buildDesc = function(tracker) {
+    $("#content").empty();
+    $("body").css('background-image', 'url(http://wallpapercave.com/wp/d4emJ2t.jpg)');
+    $("body").css('background-size', '100%');
+    $("body").css('background-repeat', 'no-repeat');
+    var h1 = $('<h1>');
+    var ul = $('<ul>');
+    var li2 = $('<li>');
+
+    h1.text(tracker.restaurantName);
+    $('body').append(h1);
+    // img.attr('src', pokemon.img);
+    // li.append(img);
+    li2.text("$" + tracker.bill);
+    ul.append(li2);
+    $('body').append(ul);
+
+    var backbutton = $('<button>');
+    backbutton.addClass('backbutton');
+    backbutton.text('Return to home');
+    backbutton.on('click', function() {
+        startUp();
+        console.log("back button clicked");
+        $("body").empty();
+        $("body").css('background-image', 'url(http://wallpapercave.com/wp/k4eop3o.jpg)');
+        $("body").css('background-position', 'center');
+        $("body").css('background-attachment', 'fixed');
+        $("body").css('background-size', 'cover');
+
+        var tablediv = $("<div>");
+        tablediv.attr('id', 'table');
+        $("#content").append(tablediv);
+    });
+    $("body").append(backbutton);
+}
